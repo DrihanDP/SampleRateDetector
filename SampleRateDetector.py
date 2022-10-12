73fe9776fe6dce320a9ed627f3b97e69e59f09c0
Drihan du Preez <drihan@btinternet.com>
2022-10-12 13:09:13 +0100

    embedded the code further

--- SampleRateDetector.py
+++ SampleRateDetector.py
@@ -10,43 +10,48 @@ line = f.readline()
 if file.endswith(".asc"):
     time = 1
     position = 0
+    id_position = 2
+    lower_bound = 0.999
+    upper_bound = 1.001
 elif file.endswith(".trc"):
     time = 100
     position = 1
+    id_position = 3
+    lower_bound = 999.9
+    upper_bound = 1000.1
 
-first_bit = []
+first_id = ''
 first_bit_count = 0
 
 while line:
     splitLine = [x for x in line.split(" ") if x != ""]
     if "Rx" not in splitLine:
         line = f.readline()
-    elif first_bit_count == 101:
-        print(splitLine[position])
-        if 0.999 < float(splitLine[position]) < 1.001:
-            print("Sample rate is 100Hz")
-        elif line[3:4] == "2":
-            print("Sample rate is 50Hz")
-        elif line[3:4] == "3" or line[3:4] == "4":
-            print("Smaple rate is 25Hz")
-        elif line[3:4] == "5":
-            print("Sample rate is 20Hz")
-        elif line[2:4] == "10":
-            print("Sample rate is 10Hz")
-        elif line[2:4] == "19" or line[2:4] == "20":
-            print("Sample rate is 5Hz")
-        elif line[2:4] == "99" or line[1:4] == "100":
-            print("Sample rate is 1Hz")
-        else:
-            print("Unknown sample rate")
-        break
     else:
-        if "Rx" in line:
-            if "301" in line[13:20]:
+        if "Rx" in splitLine:
+            if first_id in splitLine[id_position]:
                 first_bit_count += 1
-                if first_bit_count == 0:
-                    first_301_time = line[3:11]
+                if first_id == '':
+                    first_id = splitLine[id_position]
                     line = f.readline()
+                elif first_bit_count == 101:
+                    if lower_bound < float(splitLine[position]) < upper_bound:
+                        print("Sample rate is 100Hz")
+                    elif line[3:4] == "2":
+                        print("Sample rate is 50Hz")
+                    elif line[3:4] == "3" or line[3:4] == "4":
+                        print("Smaple rate is 25Hz")
+                    elif line[3:4] == "5":
+                        print("Sample rate is 20Hz")
+                    elif line[2:4] == "10":
+                        print("Sample rate is 10Hz")
+                    elif line[2:4] == "19" or line[2:4] == "20":
+                        print("Sample rate is 5Hz")
+                    elif line[2:4] == "99" or line[1:4] == "100":
+                        print("Sample rate is 1Hz")
+                    else:
+                        print("Unknown sample rate")
+                    break
                 else:
                     line = f.readline()
             else:
